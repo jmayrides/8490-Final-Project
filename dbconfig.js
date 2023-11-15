ba6558722af4f9859a92cd31b69efdd1f65b7e98
jmayrides <jmayride@villanova.edu>
2023-11-14 22:04:13 -0500

    Add team username

--- dbconfig.js
+++ dbconfig.js
@@ -5,11 +5,10 @@ Sets the information needed by Oracle to connect to the database.
 Protocol, host, port, etc. can be checked in SQLDeveloper by right
 clicking on your Oracle Connection and choosing "Properties."
 
-NEVER commit your username and password to the remote repository.
 */
 
 module.exports = {
-    user          : "username",
+    user          : "dbteam1",
     password      : "password", 
-    connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=CSDB.csc.villanova.edu)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SID=orcl)))" 
+    connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=csdb.csc.villanova.edu)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SID=orcl)))" 
 };
